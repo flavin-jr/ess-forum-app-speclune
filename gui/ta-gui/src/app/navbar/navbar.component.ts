c4c25deba5abe265500955d273893ac991728117
flavin-jr <fjcvj@cin.ufpe.br>
2022-10-17 16:25:05 -0300

    Implementação da imagem do avatar no menu lateral

--- gui/ta-gui/src/app/navbar/navbar.component.ts
+++ gui/ta-gui/src/app/navbar/navbar.component.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
-
+import { User } from '../user';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  
-  constructor() { }
   toggleMenu = false;
-  public userMaster:any;
+  constructor(private user: UserService) { }
+  post: string = '';
+  index: number = -1;
+  msgPost: boolean = true;
+  msgSucess: boolean = false;
+  allPosts:Array<string> = [];
+  userMaster:User = {
+    id: -1,
+    username: '',
+    myPosts: [],
+    comments: []
+  };
+  username:string = '';
+
   ngOnInit(): void {
-   
+    this.getUserMaster()
+
+  }
+  getUserMaster(){
+    this.user.getUser()
+    .subscribe(data=>{
+      this.userMaster =  new User(data);
+      
+      this.username = this.userMaster.username;
+      
+    })
   }
   toggle():void{
     this.toggleMenu = !this.toggleMenu;
