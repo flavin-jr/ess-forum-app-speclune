665044caf054eece3e026cb723b938be7fdca716
flavin-jr <fjcvj@cin.ufpe.br>
2022-10-17 18:33:13 -0300

    modificações de id e classes para uso no bloco de testes, alem da finalizazao dos testes

--- test-acceptance/features/criar_posts.feature
+++ test-acceptance/features/criar_posts.feature
@@ -12,14 +12,47 @@ And escrevo a frase "lol é muito bom #topdms" na caixa de texto
 And eu seleciono a opção "Postar"
 Then eu vejo na tela uma mensagem de sucesso
 And eu ainda estou na página "Meus posts"
-And eu vejo a frase "lol é muito bom #topdms" na minha lista de posts
+And eu vejo a frase "lol é muito bom #topdms" no topo da minha lista de posts
 
 Scenario: adicionar um novo post sem texto
-Given estou logado com o usuário "bebeto09"
+Given eu estou logado com o usuário "bebeto09"
 And eu estou na página "Meus posts"
-When eu seleciono a "Criar Post"
+When eu seleciono a opção "Criar Post"
 And escrevo a frase "" na caixa de texto
 And eu seleciono a opção "Postar"
 Then eu vejo na tela uma mensagem de erro
 And eu ainda estou na página "Meus posts"
 And eu nao vejo a frase "" na minha lista de posts
+
+Scenario: editar um post existente com sucesso
+Given eu estou logado com o usuário "bebeto09"
+And eu estou na página "Meus posts"
+And eu vejo uma lista de posts
+When eu seleciono, no post mais recente, a opção "editar"
+And eu substituo a frase do post por "lol é um lixo #Naojoguem"
+And eu seleciono a opção "Salvar alterações"
+Then eu vejo na tela uma mensagem de sucesso
+And eu ainda estou na página "Meus posts"
+And eu nao vejo a frase "lol é muito bom #topdms" na minha lista de posts
+And eu vejo a frase "lol é um lixo #Naojoguem" no topo da minha lista de posts
+
+Scenario: editar um post existente sem sucesso
+Given eu estou logado com o usuário "bebeto09"
+And eu estou na página "Meus posts"
+And eu vejo uma lista de posts
+When eu seleciono, no post mais recente, a opção "editar"
+And eu substituo a frase do post por ""
+And eu seleciono a opção "Salvar alterações"
+Then eu vejo na tela uma mensagem de erro
+And eu nao vejo a frase "" na minha lista de posts
+And eu vejo a frase "lol é um lixo #Naojoguem" no topo da minha lista de posts
+
+Scenario:  Cancelar a edição de um post existente 
+Given eu estou logado com o usuário "bebeto09"
+And eu estou na página "Meus posts"
+And eu vejo uma lista de posts
+When eu seleciono, no post mais recente, a opção "editar"
+And eu substituo a frase do post por "lol é top"
+And eu seleciono a opção "Cancelar"
+Then eu vejo a frase "lol é um lixo #Naojoguem" no topo da minha lista de posts
+And eu ainda estou na página "Meus posts"
--- test-acceptance/stepdefinitions/criar_post.ts
+++ test-acceptance/stepdefinitions/criar_post.ts
@@ -7,11 +7,10 @@ let expect = chai.expect;
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-function handleString(post:string):string{
+function handleString(post: string): string {
     var postInArray: string[] = post.split(' ');
-    postInArray.splice(-4,4);
+    postInArray.splice(-4, 4);
     postInArray.shift();
-    console.log(postInArray);
     post = postInArray.join(' ');
 
     return post;
@@ -20,48 +19,80 @@ defineSupportCode(function ({ Given, When, Then }) {
 
     Given(/^eu estou logado com o usuário "([^\"]*)"$/, async (username) => {
         await browser.get("http://localhost:4200/my_posts");
-        var user:string = await element(by.id('username')).getText();
+        var user: string = await element(by.id('username')).getText();
         expect(user).to.equal(username);
     })
-    
+
     Given(/^eu estou na página "([^\"]*)"$/, async (name) => {
-        
+
         await expect(browser.getTitle()).to.eventually.equal(name);
     })
+
+    Given(/^eu vejo uma lista de posts/, async () => {
+
+        await expect((await element.all(by.className('post-list'))).length).to.be.above(0);
+    })
     When(/^eu seleciono a opção "([^\"]*)"$/, async (name) => {
-        console.log(name);
         await element(by.buttonText(<string>name)).click();
     })
 
     When(/^escrevo a frase "([^\"]*)" na caixa de texto/, async (post) => {
-        console.log(post);
         await element(by.id('caixa-de-texto-post')).clear();
-        await element(by.id('caixa-de-texto-post')).sendKeys(<string> post);
-        
+        await element(by.id('caixa-de-texto-post')).sendKeys(<string>post);
+
     })
+
     When(/^eu seleciono a opção "([^\"]*)"$ /, async (name) => {
-        
+
         await element(by.buttonText(<string>name)).click();
-        
-        
+
+
     })
-    Then(/^eu vejo na tela uma mensagem de sucesso/,async()=>{
+
+    When(/^eu seleciono, no post mais recente, a opção "([^\"]*)"$/, async (name) => {
+        await element.all(by.buttonText(<string>name)).first().click();
+    })
+
+    When(/^eu substituo a frase do post por "([^\"]*)"$/, async (postEdited) => {
+
+        await element(by.id('caixa-de-texto-post')).clear();
+
+
+        await element(by.id('caixa-de-texto-post')).sendKeys(<string>postEdited);
+
+    })
+
+
+    Then(/^eu vejo na tela uma mensagem de sucesso/, async () => {
         await expect(element(by.className('msg-sucess')).isPresent()).to.eventually.equal(true);
     })
 
-    Then(/^eu ainda estou na página "([^\"]*)"$/,async(pageName)=>{
-        await expect(browser.getTitle()).to.eventually.equal(pageName);
+    Then(/^eu ainda estou na página "([^\"]*)"$/, async (pageName) => {
+        sleep(4000).then(() => {
+            expect(browser.getTitle()).to.eventually.equal(pageName);
+
+        })
 
     })
-    Then(/^eu vejo a frase "([^\"]*)" na minha lista de posts/,async(post:string)=>{
-        var mostRecentPost:string = await element.all(by.className('post-list')).first().getText();
+    Then(/^eu vejo a frase "([^\"]*)" no topo da minha lista de posts/, async (post: string) => {
+        var mostRecentPost: string = await element.all(by.className('post-list')).first().getText();
         mostRecentPost = handleString(mostRecentPost);
         expect(mostRecentPost).to.equal(post);
     })
-    Then(/^eu vejo na tela uma mensagem de erro/,async()=>{
-        await expect(element(by.className('msg-error')).isPresent()).to.eventually.equal(true);
+    Then(/^eu vejo na tela uma mensagem de erro/, async () => {
+        sleep(4000).then(() => {
+            expect(element(by.className('msg-error')).isPresent()).to.eventually.equal(true);
+        })
     })
-    
+    Then(/^eu nao vejo a frase "([^\"]*)" na minha lista de posts/, async (post: string) => {
+        await browser.get("http://localhost:4200/my_posts");
+        var mostRecentPost: string = await element.all(by.className('post-list')).first().getText();
+
+        mostRecentPost = handleString(mostRecentPost);
+        console.log(mostRecentPost);
+        expect(mostRecentPost).to.not.equal(post);
+    })
+
     // Given(/^I'm on the page "([^\"]*)"$/, async (name) => {
     //     await browser.get("http://localhost:4200/my_posts");
     //     await expect(browser.getTitle()).to.eventually.equal(name);
