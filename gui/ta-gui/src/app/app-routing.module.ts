b0f7b1499462024ce49c000a8885ef50bc7d9bc7
flavin-jr <fjcvj@cin.ufpe.br>
2022-10-17 15:01:49 -0300

    criação de titulos para as paginas com rotas diferentes

--- gui/ta-gui/src/app/app-routing.module.ts
+++ gui/ta-gui/src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { MypostsComponent } from './myposts/myposts.component';
 import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
-  { path: 'my_posts', component: MypostsComponent },
-  {path: 'edit_post', component: EditpostsComponent},
+  { path: 'my_posts', component: MypostsComponent ,title: "Meus posts"},
+  {path: 'edit_post', component: EditpostsComponent,title: "Editar posts"},
   { path: 'posts', component: PostsComponent }
 ];
 
