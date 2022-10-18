a494ec84229edcdbba7ec9f9db649243dd72e977
Renan <rgsa@cin.ufpe.br>
2022-10-17 23:46:24 -0300

    implementação da função de comentário & estilização do posts component

--- gui/ta-gui/src/app/posts/posts.component.ts
+++ gui/ta-gui/src/app/posts/posts.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 import { UserService } from '../user.service';
 import { User} from '../user';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-
-  constructor(private user: UserService) { }
-  commentStatus: boolean = false;
-
+ 
+  constructor(private user: UserService,private router:Router ) { }
+  commentStatus: Array<boolean> = [];
+  commentText:string = '';
+ 
   allPosts: Array<any>= [];
   public allUsers: Array<User>= [];
   allUsersTemp:Array<User>= [];
-  
+  allPostsSize = this.allPosts.length;
+ 
   ngOnInit(): void {
     this.getAllUser();
-  
+ 
   }
   getAllUser(){
     this.user.getPosts()
@@ -43,22 +45,45 @@ export class PostsComponent implements OnInit {
           user.comments.shift(); 
           this.allPosts.push({id:user.id, post:post ,comment:comments});
         }
+        this.commentStatus.push(false); //setando o array como falso para cada post
       }
     }
     console.log(this.allPosts);
+    console.log(this.commentStatus);
   }
   getUsernameById(id:number):string{
     const name:any = this.allUsers.find((value)=>{
-
+ 
       return value.id===id;
     });
     console.log(name.username);
     return name.username;
   }
-
-  toggleComment():void{
-    this.commentStatus = !this.commentStatus;
+ 
+  toggleComment(pos:number):void{
+    this.commentStatus[pos] = !this.commentStatus[pos];
     console.log(this.commentStatus);
   }
+ 
+  clearTextArea():void{
+    this.commentText = '';
+ 
+  }
+ 
+  addComment(id:number, post:string, idSender:number, comment:String):void{
+    const data = {id:id,post:post,idSender:idSender,comment:comment};
+ 
+    this.user.addComment(data)
+    .subscribe(res=>{
+ 
+      console.log('comentario adicionado');
 
+    });
+    this.redirectTo('/posts')
+  }
+  redirectTo(uri:string){
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=>
+    this.router.navigate([uri]));
+ }
+ 
 }
